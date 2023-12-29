bcc4436227a8aeaa5428854f3f2238a5955cb93e
dario <darioski.bassani@gmail.com>
2023-12-29 21:23:37 +0100

    29/12/23

--- approx-zeri/Form1.cs
+++ approx-zeri/Form1.cs
@@ -16,5 +16,47 @@ namespace approx_zeri
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        void plotfunzione(double x1, double x2, Funzione f)
+        {
+            double proporzionex = (x2-x1) / this.ClientSize.Width;
+            double ymax = f.Valoreinx(x1);
+            double ymin = f.Valoreinx(x1);
+            double ciuccia = x1;
+
+            for (int i = 1; i < this.ClientSize.Width; i++)
+            {
+                ciuccia += proporzionex;
+                if(f.Valoreinx(x1)>ymax)
+                {
+                    ymax=f.Valoreinx(x1);
+                }
+                if(f.Valoreinx(x1)<ymin)
+                {
+                    ymin=f.Valoreinx(x1);
+                }
+            }
+            double proprozioney = (ymax - ymin) / this.ClientSize.Height;
+
+            Graphics g = this.CreateGraphics();
+            SolidBrush pennelloCerchio = new SolidBrush(Color.Red);
+
+            for (int i=0; i < this.ClientSize.Width;i++)
+            {
+                g.FillEllipse(pennelloCerchio, i, this.ClientSize.Height - (int)Math.Round((f.Valoreinx(x1)-ymin)), 5, 5);
+                Console.WriteLine("({0},{1})", i, (int)Math.Round(f.Valoreinx(x1)));
+                x1 += proporzionex;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Funzione funzione = new Funzione();
+            funzione.FunzioneStr = "x+1";
+            plotfunzione(-5, 30, funzione);
+        }
     }
 }
--- approx-zeri/funzione.cs
+++ approx-zeri/funzione.cs
@@ -1,30 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace approx_zeri
 {
-    internal class funzione
+    internal class Funzione
     {
         public string FunzioneStr { get;set; }
 
         public double Valoreinx(double x)
         {
-            string funcConVal = this.FunzioneStr.Replace("x", x.ToString());
-            var result = new DataTable().Compute(funcConVal, null);
-            return (double)result;
+            string funcConVal = this.FunzioneStr.Replace("x", x.ToString(CultureInfo.InvariantCulture));
+            //var result = Convert.ToDouble(new DataTable().Compute(funcConVal, null));
+            var result =new DataTable().Compute(funcConVal, null);
+            return Convert.ToDouble(result);
         }
         public double Derinx(double x) 
         {
             return (this.Valoreinx(x+0.000000001)+this.Valoreinx(x))/ 0.000000001;
         }
-        public void stampafunzione(double x1, double x2) 
-        { 
-            
-        }
-
+        
     }
 }
